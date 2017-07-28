0863bee1a7c86944f18f89f3d0c0aeb665ac8f32
Roxanne Charchian <rmcharchian@gmail.com>
2017-07-28 13:33:58 -0500

    jQuery from class

---
+++ Betelgeuse/betelgeuse-jquery-selectors/scripts.js
@@ -0,0 +1,13 @@
+console.log('scripts.js was sourced');
+
+$(document).ready(function() {
+    $('body').append('<h2>Hey there!!</h2>');
+    $('body').prepend('<h2>I am going to the top!!!</h2>');
+    $('li').css('color', 'blue');
+    $('#Kevin li').css('color', 'maroon');
+    $('.food').css('color', 'pink');
+    $('#chief').css('color', 'green');
+    //$('#Kevin li:last-child').css('color', 'black');
+    $('ul').first().children().last().css('color','black');
+});
+
